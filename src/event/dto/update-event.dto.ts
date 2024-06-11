be33b6553f7b41a58ec541bd1dff46394e364301
saipepu <saipepu.mdy257@gmail.com>
2024-06-11 21:37:42 +0700

    update api docs

--- src/event/dto/update-event.dto.ts
+++ src/event/dto/update-event.dto.ts
@@ -4,15 +4,25 @@ import { Status } from "../schema/event.schema";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class UpdateEventDto {
-  @ApiProperty({ example: "Event Name", description: "Name of the event" })
+  @ApiProperty({ example: "Event Name", description: "Name of the event." })
   @IsOptional()
   readonly name: string;
 
-  @ApiProperty({ example: "Event Description", description: "Description of the event" })
+  @ApiProperty({ example: "Event Description", description: "Description of the event."})
+  @IsOptional()
+  readonly description: string;
+
+  @ApiProperty({ example: "2022-12-31", description: "Event Start Date."})
+  @IsOptional()
+  readonly startDate: string;
+
+  @ApiProperty({ example: "2022-12-31", description: "Event End Date."})
+  @IsOptional()
+  readonly endDate: string;
+
   @IsEmpty({ message: "You cannot pass organizer Id"})
   readonly organizer: Organizer;
 
-  @ApiProperty({ description: "Don't pass status. Use update-event-status api. Ask developer for more info."})
   @IsEmpty({ message: "You cannot pass Status. Use update-event-status api. Ask developer for more info."})
   readonly status: Status;
 }
