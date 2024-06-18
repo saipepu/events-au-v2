97168094ea264771a6d3238fac7f82b788702774
saipepu <saipepu.mdy257@gmail.com>
2024-06-18 23:45:55 +0700

    update create-event-dto

--- src/event/dto/create-event.dto.ts
+++ src/event/dto/create-event.dto.ts
@@ -1,3 +1,4 @@
+import { Optional } from "@nestjs/common";
 import { ApiProperty } from "@nestjs/swagger";
 import { IsEmpty, IsNotEmpty } from "class-validator";
 
@@ -6,8 +7,37 @@ export class CreateEventDto {
   @IsNotEmpty()
   readonly name: string;
 
-  @IsEmpty({ message: "You cannot pass organizer Id"})
-  readonly organizer: string;
+  @ApiProperty({ example: "Event Description", description: "Description of the event" })
+  @Optional()
+  readonly description: string;
+
+  @ApiProperty({ example: "Event Location", description: "Location of the event" })
+  @Optional()
+  readonly location: string;
+
+  @ApiProperty({ example: "2021-12-31T23:59:59.999Z", description: "Start date of the event" })
+  @Optional()
+  readonly startDate: Date;
+
+  @ApiProperty({ example: "2021-12-31T23:59:59.999Z", description: "End date of the event" })
+  @Optional()
+  readonly endDate: Date;
+
+  @ApiProperty({ example: "2021-12-31T23:59:59.999Z", description: "Start time of the event" })
+  @Optional()
+  readonly startTime: Date;
+
+  @ApiProperty({ example: "2021-12-31T23:59:59.999Z", description: "End time of the event" })
+  @Optional()
+  readonly endTime: Date;
+
+  @ApiProperty({ example: "No Alcohol", description: "Restrictions of the event" })
+  @Optional()
+  readonly rules: string;
+
+  @ApiProperty({ example: "https://www.image.com", description: "Image URL of the event" })
+  @Optional()
+  readonly imageUrl: string;
 
   @ApiProperty({ example: "<mongoose _id>", description: "Unit ID" })
   @IsNotEmpty({ message: "An Event should belonge to at least one unit."})
