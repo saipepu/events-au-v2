4586f9e4ddbb06946e7529a250d71a2197b32628
Tanat Arora <tanat.navin@gmail.com>
2024-07-23 19:05:19 +0700

    add poll result in get participant by eventId

--- src/poll-result/poll-result.controller.ts
+++ src/poll-result/poll-result.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { PollResultService } from './poll-result.service';
 import { CreatePollResultDto } from './dto/create-poll-result.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('poll-result')
 @Controller('poll-result')
 export class PollResultController {
   constructor(private readonly pollResultService: PollResultService) {}
