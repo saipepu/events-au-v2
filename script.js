31f3b73e1390bfc338654d1c6e0d3fb17cacf8ca
saipepu <saipepu.mdy257@gmail.com>
2024-09-12 12:39:39 +0700

    Swagger Docs Update

--- script.js
+++
@@ -1,9 +0,0 @@
-function validateTime(time) {
-  const timeRegex = /^$|^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
-  return timeRegex.test(time);
-}
-
-// Test cases
-console.log(validateTime(""));         // true
-console.log(validateTime(null));       // false (you might want to handle null separately)
-console.log(validateTime("12:34"));
