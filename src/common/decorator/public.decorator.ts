a2f3bad523a01477ba4e768f42f772718952b544
saipepu <saipepu.mdy257@gmail.com>
2025-08-15 23:00:51 +0700

    feat(auth): add Public decorator for public route metadata

---
+++ src/common/decorator/public.decorator.ts
@@ -0,0 +1,5 @@
+import { createParamDecorator, ExecutionContext, SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
