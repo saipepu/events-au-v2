ca3d2dca708011439e7f9628c9b435ac9c48cba7
saipepu <saipepu.mdy257@gmail.com>
2025-08-15 23:00:31 +0700

    refactor(auth): update SignUpDto to use optional properties and remove unused fields

--- src/auth/dto/signUp.dto.ts
+++ src/auth/dto/signUp.dto.ts
@@ -1,29 +1,12 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class SignUpDto {
-  @ApiProperty({ example: 'John', description: 'First Name' })
-  @IsNotEmpty()
-  @IsString()
-  readonly firstName: string
+  @ApiPropertyOptional({ example: 'John', description: 'First Name' })
+  readonly firstName?: string
 
   @ApiProperty({ example: 'Doe@gmail.com', description: 'Last Name' })
   @IsNotEmpty()
   @IsEmail({}, { message: 'Please enter correct email.'})
   readonly email: string
-
-  @ApiProperty({ example: '123456788', description: 'Phone Number' })
-  @IsNotEmpty()
-  @IsNumber({}, { message: 'Phone number must be a number.'})
-  readonly phone: number
-
-  @ApiProperty({ example: '<mongoose _id>', description: 'Unit ID' })
-  @IsOptional()
-  @IsString()
-  readonly unitId: string
-
-  @ApiProperty({ example: 'password', description: 'Password' })
-  @IsNotEmpty()
-  @IsString()
-  readonly password: string
 }
